c570933e868f5abd9c077581ce01891e64644175
yameendigitech <112205719+yameendigitech@users.noreply.github.com>
2022-08-30 13:51:50 -0700

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,508 @@
+function awaitpl() {
+window.msgs.innerHTML="<h1 style=color:#87FF33>"+LoadedMSG+"</h1>";
+}
+
+function load_exploit(){
+msgs.innerHTML="Loading Jailbreak... Please Wait !!!..";
+setTimeout(poc, 1500);
+}
+
+function load_exploit_done(){
+msgs.innerHTML="Jailbreak Loaded !!!";
+}
+
+function load_miraloader(){
+var script = document.createElement('script');script.src = "mira.js";document.getElementsByTagName('head')[0].appendChild(script);
+msgs.innerHTML="Payload Loading... Please Wait !!!";
+LoadedMSG="Payload Loaded... Press OK Now !!!";
+exec_type = "payload";
+setTimeout(poc,1500);
+}
+
+function load_mira(){
+var script = document.createElement('script');script.src = "mira2.js";document.getElementsByTagName('head')[0].appendChild(script);
+msgs.innerHTML="Loading Jailbreak + Mira Orbis... Please Wait !!!";
+LoadedMSG="Mira Orbis Loaded... Press OK Now !!!";
+exec_type = "payload";
+setTimeout(poc,1500);
+}
+
+function load_goldhen212(){
+msgs.innerHTML="Loading Jailbreak + GoldHEN v2.1.2 ... Please Wait !!!";
+LoadedMSG="GoldHEN v2.1.2 Loaded... Press OK Now !!!";
+PLfile = "goldhen212.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_goldhen222(){
+msgs.innerHTML="Loading Jailbreak + GoldHEN v2.2.2 ... Please Wait !!!";
+LoadedMSG="GoldHEN v2.2.2 Loaded... Press OK Now !!!";
+PLfile = "goldhen222.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_goldhen23b2(){
+msgs.innerHTML="Loading Jailbreak + GoldHEN v2.3b2 ... Please Wait !!!";
+LoadedMSG="GoldHEN v2.3b2 Loaded... Press OK Now !!!";
+PLfile = "goldhen23b2.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtabeef133(){
+msgs.innerHTML="Loading Jailbreak + GTA V Beef Queef v1.33... Please Wait !!!";
+LoadedMSG="GTA V Beef Queef v1.33 Loaded... Press OK Now !!!";
+PLfile = "BeefQueefMod133.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtabeef134(){
+msgs.innerHTML="Loading Jailbreak + GTA V Beef Queef v1.34... Please Wait !!!";
+LoadedMSG="GTA V Beef Queef v1.34 Loaded... Press OK Now !!!";
+PLfile = "BeefQueefMod134.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtabeef138(){
+msgs.innerHTML="Loading Jailbreak + GTA V Beef Queef v1.38... Please Wait !!!";
+LoadedMSG="GTA V Beef Queef v1.38 Loaded... Press OK Now !!!";
+PLfile = "BeefQueefMod138.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gta100(){
+msgs.innerHTML="Loading Jailbreak + GTA V Arabic Guy v1.00... Please Wait !!!";
+LoadedMSG="GTA V ArabicGuy v1.00 Loaded... Press OK Now !!!";
+PLfile = "gtav_ar100.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gta127(){
+msgs.innerHTML="Loading Jailbreak + GTA V Arabic Guy v1.27... Please Wait !!!";
+LoadedMSG="GTA V ArabicGuy v1.27 Loaded... Press OK Now !!!";
+PLfile = "arabicguy127.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gta132(){
+msgs.innerHTML="Loading Jailbreak + GTA V Arabic Guy v1.32... Please Wait !!!";
+LoadedMSG="GTA V ArabicGuy v1.32 Loaded... Press OK Now !!!";
+PLfile = "arabicguy132.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gta133(){
+msgs.innerHTML="Loading Jailbreak + GTA V Arabic Guy v1.33... Please Wait !!!";
+LoadedMSG="GTA V ArabicGuy v1.33 Loaded... Press OK Now !!!";
+PLfile = "arabicguy133.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtawildmodz132(){
+msgs.innerHTML="Loading Jailbreak + GTA V WildeModz v1.32... Please Wait !!!";
+LoadedMSG="GTA V WildeModz v1.32 Loaded... Press OK Now !!!";
+PLfile = "wildemodz132.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtawildmodz133(){
+msgs.innerHTML="Loading Jailbreak + GTA V WildeModz v1.33... Please Wait !!!";
+LoadedMSG="GTA V WildeModz v1.33 Loaded... Press OK Now !!!";
+PLfile = "wildemodz133.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtawildmodz138(){
+msgs.innerHTML="Loading Jailbreak + GTA V WildeModz v1.38... Please Wait !!!";
+LoadedMSG="GTA V WildeModz v1.38 Loaded... Press OK Now !!!";
+PLfile = "wildemodz138.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtaexpulsion138(){
+msgs.innerHTML="Loading Jailbreak + GTA V Expulsion v1.38... Please Wait !!!";
+LoadedMSG="GTA V Expulsion v1.38 Loaded... Press OK Now !!!";
+PLfile = "gtavexpulsion138.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gtalamance100b(){
+    msgs.innerHTML="Loading Jailbreak + GTA V Lamance v1.00 Beta... Please Wait !!!";
+    LoadedMSG="GTA V Lamance v1.00 Beta Loaded... Press OK Now !!!";
+    PLfile = "GTAVLamance100b.bin";
+    exec_type = "payload";
+    toogle_payload();
+}
+
+function load_gtalamance138v09(){
+msgs.innerHTML="Loading Jailbreak + GTA V Lamance v1.38 ( Payload version 0.9 )... Please Wait !!!";
+LoadedMSG="GTA V Lamance v1.38 ( Payload version 0.9 ) Loaded... Press OK Now !!!";
+PLfile = "gtavlamance138v09.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rdr2100(){
+msgs.innerHTML="Loading Red Dead Redemption II Mod V1.3.7 Beta for Update v1.00 ... Please Wait !!!";
+LoadedMSG="Red Dead Redemption II Mod V1.3.7 Beta for Update v1.00 Loaded";
+PLfile = "OystersMenu100.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rdr2113(){
+msgs.innerHTML="Loading Red Dead Redemption II Mod v1.3.6 for Update v1.13 ... Please Wait !!!";
+LoadedMSG="Red Dead Redemption II Mod v1.3.6 for Update v1.13 Loaded";
+PLfile = "OystersMenu113.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rdr2119(){
+msgs.innerHTML="Loading Red Dead Redemption II Mod v1.3.6 for Update v1.19 ... Please Wait !!!";
+LoadedMSG="Red Dead Redemption II Mod v1.3.6 for Update v1.19 Loaded";
+PLfile = "OystersMenu119.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rdr2124(){
+msgs.innerHTML="Loading Red Dead Redemption II Mod V1.3.7 Beta for Update v1.24 ... Please Wait !!!";
+LoadedMSG="Red Dead Redemption II Mod V1.3.7 Beta for Update v1.24 Loaded";
+PLfile = "OystersMenu124.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rdr2129(){
+msgs.innerHTML="Loading Red Dead Redemption II Mod V1.3.7 Beta for Update v1.29 ... Please Wait !!!";
+LoadedMSG="Red Dead Redemption II Mod V1.3.7 Beta for Update v1.29 Loaded";
+PLfile = "OystersMenu129.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_todex(){
+msgs.innerHTML="Loading Jailbreak + ToDex... Please Wait !!!";
+LoadedMSG="ToDex Loaded... Press OK Now !!!";
+PLfile = "todex.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_kerneldumper(){
+msgs.innerHTML="Loading Jailbreak + Kernel Dumper... Please Wait !!!";
+LoadedMSG="Kernel Dumper Loaded... Press OK Now !!!";
+PLfile = "kerneldumper.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_todexon(){
+msgs.innerHTML="Loading Jailbreak + ToDex Enable... Please Wait !!!";
+LoadedMSG="ToDex Enable Loaded... Press OK Now !!!";
+PLfile = "todex_enable.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_todexoff(){
+msgs.innerHTML="Loading Jailbreak + ToDex Disable... Please Wait !!!";
+LoadedMSG="ToDex Disable Loaded... Press OK Now !!!";
+PLfile = "todex_disable.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_gamedumper(){
+msgs.innerHTML="Loading Jailbreak + Game Dumper... Please Wait !!!";
+LoadedMSG="Game Dumper Loaded... Press OK Now !!!";
+PLfile = "DumperG.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_updatedumper(){
+msgs.innerHTML="Loading Jailbreak + Update Dumper... Please Wait !!!";
+LoadedMSG="Update Dumper Loaded... Press OK Now !!!";
+PLfile = "DumperU.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_mgu(){
+msgs.innerHTML="Loading Jailbreak + MGU Dumper... Please Wait !!!";
+LoadedMSG="MGU Dumper Loaded... Press OK Now !!!";
+PLfile = "DumperMGU.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_sgu(){
+msgs.innerHTML="Loading Jailbreak + SGU Dumper... Please Wait !!!";
+LoadedMSG="SGU Dumper Loaded... Press OK Now !!!";
+PLfile = "DumperSGU.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_pkgbackup(){
+msgs.innerHTML="Loading Jailbreak + PKG Backup v1.1... Please Wait !!!";
+LoadedMSG="PKG Backup Loaded... Press OK Now !!!";
+PLfile = "ps4_pkg_backup11.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_ftp(){
+msgs.innerHTML="Loading Jailbreak + FTP... Please Wait !!!";
+LoadedMSG="FTP Loaded... Press OK Now !!!";
+PLfile = "ftp.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_app2usb(){
+msgs.innerHTML="Loading Jailbreak + AppUsb... Please Wait !!!";
+LoadedMSG="AppUsb Loaded... Press OK Now !!!";
+PLfile = "app2usb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_webrte(){
+msgs.innerHTML="Loading Jailbreak + WebRte... Please Wait !!!";
+LoadedMSG="WebRte Loaded... Press OK Now !!!";
+PLfile = "webrte.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_ps4debug(){
+msgs.innerHTML="Loading Jailbreak + Ps4Debug... Please Wait !!!";
+LoadedMSG="Ps4Debug Loaded... Press OK Now !!!";
+PLfile = "ps4debug.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_dbackup(){
+msgs.innerHTML="Loading Jailbreak + Ps4Debug... Please Wait !!!";
+LoadedMSG="DB Backup Loaded... Press OK Now !!!";
+PLfile = "backup.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_dbrestore(){
+msgs.innerHTML="Loading Jailbreak + DB Restore... Please Wait !!!";
+LoadedMSG="DB Restore Loaded... Press OK Now !!!";
+PLfile = "restore.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_enablebUpdates(){
+msgs.innerHTML="Loading Jailbreak + Enable Updates... Please Wait !!!";
+LoadedMSG="Enable Updates Loaded... Press OK Now !!!";
+PLfile = "enableupdate.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_disableUpdates(){
+msgs.innerHTML="Loading Jailbreak + Disable Updates... Please Wait !!!";
+LoadedMSG="Disable Updates Loaded... Press OK Now !!!";
+PLfile = "disableupdates.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_historyBlocker(){
+msgs.innerHTML="Loading Jailbreak + History Blocker... Please Wait !!!";
+LoadedMSG="History Blocker Loaded... Press OK Now !!!";
+PLfile = "historyblocker.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_kernelClock(){
+msgs.innerHTML="Loading Jailbreak + Kernel Clock... Please Wait !!!";
+LoadedMSG="Kernel Clock Loaded... Press OK Now !!!";
+PLfile = "kernelclock.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_fanthreshold(){
+var script = document.createElement('script');script.src = "fanthreshold.js";localStorage.setItem('fanthreshold', tempC.value);document.getElementsByTagName('head')[0].appendChild(script);
+msgs.innerHTML="Loading Jailbreak + Fan Threshold... Please Wait !!!";
+LoadedMSG="Fan Threshold Loaded... Press OK Now !!!";
+exec_type = "payload";
+setTimeout(poc,1500);
+}
+
+function load_webactivator(){
+var script = document.createElement('script');script.src = "webactivator.js";document.getElementsByTagName('head')[0].appendChild(script);
+msgs.innerHTML="Loading Jailbreak + Web Activator... Please Wait !!!";
+LoadedMSG="Web Activator Loaded... Press OK Now !!!";
+exec_type = "payload";
+setTimeout(poc,1500);
+}
+
+function load_linux1(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 1gb By Ps3ita... Please Wait !!!";
+LoadedMSG="Linux Loader 1gb By Ps3ita Loaded... Press OK Now !!!";
+PLfile = "linuxloader1gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_linux2(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 2gb By Ps3ita... Please Wait !!!";
+LoadedMSG="Linux Loader 2gb By Ps3ita Loaded... Press OK Now !!!";
+PLfile = "linuxloader2gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_linux3(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 3gb By Ps3ita... Please Wait !!!";
+LoadedMSG="Linux Loader 3gb By Ps3ita Loaded... Press OK Now !!!";
+PLfile = "linuxloader3gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_linux4(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 4gb By Ps3ita... Please Wait !!!";
+LoadedMSG="Linux Loader 4gb By Ps3ita Loaded... Press OK Now !!!";
+PLfile = "linuxloader4gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_linux5(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 5gb By Ps3ita... Please Wait !!!";
+LoadedMSG="Linux Loader 5gb By Ps3ita Loaded... Press OK Now !!!";
+PLfile = "linuxloader5gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_nlinux1(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 1gb... Please Wait !!!";
+LoadedMSG="Linux Loader 1gb Loaded... Press OK Now !!!";
+PLfile = "LinuxLoader_n1gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_nlinux2(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 2gb... Please Wait !!!";
+LoadedMSG="Linux Loader 2gb Loaded... Press OK Now !!!";
+PLfile = "LinuxLoader_n2gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_nlinux3(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 3gb... Please Wait !!!";
+LoadedMSG="Linux Loader 3gb Loaded... Press OK Now !!!";
+PLfile = "LinuxLoader_n3gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_nlinux4(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 4gb... Please Wait !!!";
+LoadedMSG="Linux Loader 4gb Loaded... Press OK Now !!!";
+PLfile = "LinuxLoader_n4gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_nlinux5(){
+msgs.innerHTML="Loading Jailbreak + Linux Loader 5gb... Please Wait !!!";
+LoadedMSG="Linux Loader 5gb Loaded... Press OK Now !!!";
+PLfile = "LinuxLoader_n5gb.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_WhosThatSouthbridge(){
+msgs.innerHTML="Loading Jailbreak + Whos That Southbridge ( Linux Payload )... Please Wait !!!";
+LoadedMSG="Whos That Southbridge ( Linux Payload ) Loaded... Press OK Now !!!";
+PLfile = "whos_that_southbridge_2.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_exitidu(){
+msgs.innerHTML="Loading Jailbreak + Exit idu... Please Wait !!!";
+LoadedMSG="Exit idu Loaded... Press OK Now !!!";
+PLfile = "exitidu.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_ps4parentalcontrols(){
+msgs.innerHTML="Loading Jailbreak + Parental Controls... Please Wait !!!";
+LoadedMSG="Parental Controls Loaded... Press OK Now !!!";
+PLfile = "ps4parentalcontrols.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_moduleDump(){
+msgs.innerHTML="Loading Jailbreak + Module Dumper... Please Wait !!!";
+LoadedMSG="Module Dumper Loaded... Press OK Now !!!";
+PLfile = "moduledumper.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_orbistoolbox(){
+msgs.innerHTML="Loading Jailbreak + Orbis Toolbox Alfa v1190... Please Wait !!!";
+LoadedMSG="Orbis Toolbox Alfa v1190 Loaded... Press OK Now !!!";
+PLfile = "OrbisToolbox900.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_disbAslr(){
+msgs.innerHTML="Loading Jailbreak + Disable ASLR... Please Wait !!";
+LoadedMSG="Disable ASLR Loaded... Press OK Now !!!";
+PLfile = "daslr.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_rifRen(){
+msgs.innerHTML="Loading Jailbreak + RIF Renamer... Please Wait !!!";
+LoadedMSG="RIF Renamer Loaded... Press OK Now !!!";
+PLfile = "rifren.bin";
+exec_type = "payload";
+toogle_payload();
+}
+
+function load_permUart(){
+msgs.innerHTML="Loading Jailbreak + Permanent UART... Please Wait !!!";
+LoadedMSG="Permanent UART Loaded... Press OK Now !!!";
+PLfile = "puart.bin";
+exec_type = "payload";
+toogle_payload();
+}
